de647a047c993c3e18887f1c47c714e0364a9ab8
Theo Vora <theo.vora@gmail.com>
2020-04-10 10:19:35 -0600

    Create person model

---
+++ Pairs/Model/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Pairs
+//
+//  Created by Theo Vora on 4/10/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+}
